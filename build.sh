afb83d1f707adbe78f45ce2581d38cc6a25bf2b5
Eshwar-21at1a3529 <erao2303@gmail.com>
2026-04-17 00:55:14 +0530

    fix: add python-decouple for Render deployment

--- build.sh
+++ build.sh
@@ -6,3 +6,11 @@ pip install -r backend/requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r backend/requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
